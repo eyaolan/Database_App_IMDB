dd7e18d3bc4a325cbf7852a924d5c95ea6fcacfb
yaolan <eyaolan@gmail.com>
2017-05-19 23:41:53 -0700

    insert two rows successfully

--- src/Populate.java
+++ src/Populate.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
 
 /**
  * Created by yaolan on 5/17/17.
  */
 public class Populate {
+
+    static final String url = "jdbc:oracle:thin:@localhost:49161:xe";
+    static final String user = "system";
+    static final String password = "oracle";
 
     //path of data file
     private static final String MOVIES = "/Users/yaolan/Documents/movieDB/movies.dat";
@@ -28,31 +33,89 @@ public class Populate {
     private static final String TAB_VALUE = (Character.toString((char) 9));
 
 
-    private void insert(String line, String tableName){
-        String values = line.replaceAll(TAB_VALUE, "','");
-        values = "'"+values+"'";
-        String sql = SQL_INSERT.replaceFirst(TABLE_REGEX,tableName);
-        sql = sql.replaceFirst(VALUES_REGEX,values);
-        System.out.println(sql);
-        Hw3.connectDB(sql);
+    public void insertToDB(String filePath, String tableName) throws IOException {
+
+        //initial the resultSet, connection and statement
+        ResultSet resultSet = null;
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+
+            //connect to DB
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected database successfully...");
+
+            //executeQuery
+            stmt = conn.createStatement();
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+            String line = "";
+            String keys = "";
+            int i = 0;
+            if ((line = reader.readLine()) != null) {
+                keys = line.replaceAll(TAB_VALUE, ",");
+                //System.out.print(keys);
+            }
+
+            try {
+                while ((line = reader.readLine()) != null && i < 2) {
+                    String values = line.replaceAll(TAB_VALUE, "','");
+                    values = "'" + values + "'";
+                    String sql = SQL_INSERT.replaceFirst(TABLE_REGEX, tableName);
+                    sql = sql.replaceFirst(VALUES_REGEX, values);
+
+                    //stmt.executeUpdate(sql);
+                    stmt.executeQuery(sql);
+                    System.out.print(i);
+                    i++;
+                }
+            }catch(Exception e){
+                    try {
+                        conn.rollback();
+                    } catch (SQLException sqle) {
+                        System.out.println("There is a error in rollback!");
+                    }
+                } finally{
+                    try {
+                        conn.commit();
+                        conn.close();
+                        System.out.println("Disconnect to Database");
+                    } catch (SQLException sqle) {
+                        System.out.println("There is a error in commit!");
+                    }
+                }
+
+            reader.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    conn.close();
+            } catch (SQLException se) {
+            }// do nothing
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
     }
+
 
     public static void main(String[] agrs) throws IOException {
 
         Populate populate = new Populate();
-        BufferedReader reader = new BufferedReader(new FileReader(MOVIES));
+        populate.insertToDB(MOVIES, "MOVIE");
 
-        String line = "";
-        String keys = "";
-        int i = 0;
-        if ((line = reader.readLine())!=null ){
-            keys = line.replaceAll(TAB_VALUE,",");
-        }
 
-        while ((line = reader.readLine()) != null && i<1) {
-            populate.insert(line,"MOVIE");
-            i++;
-        }
-        reader.close();
     }
+
+
 }
